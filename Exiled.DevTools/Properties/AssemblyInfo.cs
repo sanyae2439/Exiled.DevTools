606b56c0106e8543f91fef747f7b6d32f14239f9
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2021-02-02 07:21:04 +0900

    version bump

--- Exiled.DevTools/Properties/AssemblyInfo.cs
+++ Exiled.DevTools/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      ビルド番号
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
