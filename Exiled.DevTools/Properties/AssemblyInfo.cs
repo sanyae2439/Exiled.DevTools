cd9377b7656984ab2a0ac8011f66460bf8387773
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2021-02-02 04:57:47 +0900

    2021*

--- Exiled.DevTools/Properties/AssemblyInfo.cs
+++ Exiled.DevTools/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("sanyae2439")]
 [assembly: AssemblyProduct("Exiled.DevTools")]
-[assembly: AssemblyCopyright("Copyright © sanyae2439 2020")]
+[assembly: AssemblyCopyright("Copyright © sanyae2439 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
