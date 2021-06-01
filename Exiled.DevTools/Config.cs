cc753d9964119b994145ac4c06edcface06e04be
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2021-06-02 01:13:31 +0900

    1.3.0(more effective codes)

--- Exiled.DevTools/Config.cs
+++ Exiled.DevTools/Config.cs
@@ -9,13 +9,16 @@ namespace Exiled.DevTools
 		[Description("Indicates whether the plugin is enabled or not")]
 		public bool IsEnabled { get; set; } = true;
 
+		[Description("Indicates whether logging events args")]
+		public bool LoggingEventArgs { get; set; } = true;
+
 		[Description("Ignored event names for logging")]
-		public List<string> DisabledLoggingEvents { get; set; } = new List<string>() { "SyncingData" };
+		public List<string> DisabledLoggingEvents { get; set; } = new List<string>() { "SyncingData", "Blinking", "ChangingDurability", "UsingRadioBattery" };
 
 		[Description("Ignored network method names(Rpc,Cmd,Target) for logging")]
-		public List<string> DisabledLoggingNetworkMethods { get; set; } = new List<string>() { "RpcBlinkTime", "CmdAltIsActive", "CmdSyncItem", "CmdSyncData" };
+		public List<string> DisabledLoggingNetworkMethods { get; set; } = new List<string>() { "RpcBlinkTime", "CmdAltIsActive", "CmdSyncItem", "CmdSyncData", "CmdChangeSpeedState", "CmdScp939Noise" };
 
-		[Description("Ignored nest output for logging")]
-		public List<string> DisabledLoggingClassNameForNest { get; set; } = new List<string>();
+		[Description("Ignored nest output for logging (Must be Fullname")]
+		public List<string> DisabledLoggingClassNameForNest { get; set; } = new List<string>() { "Exiled.API.Features.Player" };
 	}
 }
--- Exiled.DevTools/DevTools.cs
+++ Exiled.DevTools/DevTools.cs
@@ -18,12 +18,13 @@ namespace Exiled.DevTools
 		public override string Prefix => "exiled_devtools";
 		public override PluginPriority Priority => PluginPriority.Highest;
 		public override Version Version => new Version(Assembly.GetName().Version.Major, Assembly.GetName().Version.Minor, Assembly.GetName().Version.Build);
-		public override Version RequiredExiledVersion => new Version(2, 8, 0);
+		public override Version RequiredExiledVersion => new Version(2, 10, 0);
 
 		public static DevTools Instance { get; private set; }
 		public Harmony Harmony { get; private set; }
 
 		private readonly Dictionary<EventInfo, Delegate> _DynamicHandlers = new Dictionary<EventInfo, Delegate>();
+		private const BindingFlags _NestSearchFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly;
 
 		public override void OnEnabled()
 		{
@@ -41,7 +42,7 @@ namespace Exiled.DevTools
 
 			RemoveEventHandlers();
 			UnRegistPatch();
-	
+
 			base.OnDisabled();
 		}
 
@@ -50,8 +51,6 @@ namespace Exiled.DevTools
 			foreach(var eventClass in Events.Events.Instance.Assembly.GetTypes().Where(x => x.Namespace == "Exiled.Events.Handlers"))
 				foreach(EventInfo eventInfo in eventClass.GetEvents())
 				{
-					if(this.Config.DisabledLoggingEvents.Contains(eventInfo.Name)) continue;
-
 					Delegate handler = null;
 					if(eventInfo.EventHandlerType.GenericTypeArguments.Any())
 						handler = typeof(DevTools)
@@ -106,61 +105,66 @@ namespace Exiled.DevTools
 
 		public static void MessageHandler<T>(T ev) where T : EventArgs
 		{
-			string message = $"[{ev.GetType().Name.Replace("EventArgs", string.Empty)}]\n";
-			foreach(var propertyInfo in ev.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+			string eventname = ev.GetType().Name.Replace("EventArgs", string.Empty);
+			if(Instance.Config.DisabledLoggingEvents.Contains(eventname)) return;
+
+			string message = $"[{eventname}]\n";
+			if(Instance.Config.LoggingEventArgs)
 			{
-				try
+				foreach(var propertyInfo in ev.GetType().GetProperties())
 				{
-					message += $"{propertyInfo.Name} : {propertyInfo.GetValue(ev)}\n";
-				}
-				catch(Exception e)
-				{
-					message += $"{propertyInfo.Name} : Error[{e.Message}]\n";
-				}
-
-				if(DevTools.Instance.Config.DisabledLoggingClassNameForNest.Contains(propertyInfo.PropertyType.FullName)) continue;
-
-				if(propertyInfo.PropertyType.IsClass || (propertyInfo.PropertyType.IsValueType && !propertyInfo.PropertyType.IsPrimitive && !propertyInfo.PropertyType.IsEnum)) 
-				{
-					
-					bool isString = propertyInfo.PropertyType.Name == nameof(System.String);
-					bool isEnumerable = propertyInfo.PropertyType.GetInterfaces().Any(t => t.IsConstructedGenericType && t.GetGenericTypeDefinition() == typeof(IEnumerable<>));
-
-					if(!isString && !isEnumerable)
+					try
 					{
-						foreach(var propertyInClass in propertyInfo.PropertyType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
-						{
-							if(propertyInClass.GetIndexParameters().Length > 0) continue;
-
-							try
-							{
-								message += $"    {propertyInClass.Name} : {propertyInClass.GetValue(propertyInfo.GetValue(ev))}\n";
-
-							}
-							catch(Exception)
-							{
-								message += $"    {propertyInClass.Name} : null\n";
-							}
-						}
-
-						foreach(var fieldInClass in propertyInfo.PropertyType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
-						{
-							try
-							{
-								message += $"    {fieldInClass.Name} : {fieldInClass.GetValue(propertyInfo.GetValue(ev))}\n";
-							}
-							catch(Exception)
-							{
-								message += $"    {fieldInClass.Name} : null\n";
-							}
-						}
+						message += $"{propertyInfo.Name} : {propertyInfo.GetValue(ev)}\n";
+					}
+					catch(Exception e)
+					{
+						message += $"{propertyInfo.Name} : Error[{e.Message}]\n";
 					}
 
-					if(isEnumerable && !isString)
+					if(DevTools.Instance.Config.DisabledLoggingClassNameForNest.Contains(propertyInfo.PropertyType.FullName)) continue;
+
+					if(propertyInfo.PropertyType.IsClass || (propertyInfo.PropertyType.IsValueType && !propertyInfo.PropertyType.IsPrimitive && !propertyInfo.PropertyType.IsEnum))
 					{
-						int counter = 0;
-						foreach(var item in (IEnumerable)propertyInfo.GetValue(ev))
-							message += $"    [{counter++}] : {item}\n";
+
+						bool isString = propertyInfo.PropertyType.Name == nameof(System.String);
+						bool isEnumerable = propertyInfo.PropertyType.GetInterfaces().Any(t => t.IsConstructedGenericType && t.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+
+						if(!isString && !isEnumerable)
+						{
+							foreach(var propertyInClass in propertyInfo.PropertyType.GetProperties(_NestSearchFlags))
+							{
+								if(propertyInClass.GetIndexParameters().Length > 0) continue;
+
+								try
+								{
+									message += $"    {propertyInClass.Name} : {propertyInClass.GetValue(propertyInfo.GetValue(ev))}\n";
+								}
+								catch(Exception)
+								{
+									message += $"    {propertyInClass.Name} : null\n";
+								}
+							}
+
+							foreach(var fieldInClass in propertyInfo.PropertyType.GetFields(_NestSearchFlags))
+							{
+								try
+								{
+									message += $"    {fieldInClass.Name} : {fieldInClass.GetValue(propertyInfo.GetValue(ev))}\n";
+								}
+								catch(Exception)
+								{
+									message += $"    {fieldInClass.Name} : null\n";
+								}
+							}
+						}
+
+						if(isEnumerable && !isString)
+						{
+							int counter = 0;
+							foreach(var item in (IEnumerable)propertyInfo.GetValue(ev))
+								message += $"    [{counter++}] : {item}\n";
+						}
 					}
 				}
 			}
