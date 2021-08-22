f516edd55d0f94f75cd825fc76ff068a16eaa6b2
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2021-08-22 22:18:55 +0900

    2.0.2(Add message logging)

--- Exiled.DevTools/Config.cs
+++ Exiled.DevTools/Config.cs
@@ -15,8 +15,8 @@ namespace Exiled.DevTools
 		[Description("Ignored event names for logging")]
 		public List<string> DisabledLoggingEvents { get; set; } = new List<string>() { "SyncingData", "Blinking", "ChangingDurability", "UsingRadioBattery" };
 
-		[Description("Ignored network method names(Rpc,Cmd,Target) for logging")]
-		public List<string> DisabledLoggingNetworkMethods { get; set; } = new List<string>() { "CmdAltIsActive", "CmdSyncData", "CmdChangeSpeedState", "CmdScp939Noise", "CmdSetTime" };
+		[Description("Ignored network method names(Rpc,Cmd,Target,Message) for logging")]
+		public List<string> DisabledLoggingNetworkMethods { get; set; } = new List<string>() { "SpawnMessage", "ObjectDestroyMessage", "NetworkPingMessage", "PositionMessage", "PositionMessage2D", "RotationMessage", "CmdAltIsActive", "CmdSyncData", "CmdChangeSpeedState", "CmdScp939Noise", "CmdSetTime", "RpcMakeSound" };
 
 		[Description("Ignored nest output for logging (Must be Fullname)")]
 		public List<string> DisabledLoggingClassNameForNest { get; set; } = new List<string>() { "Exiled.API.Features.Player", "UnityEngine.Vector3", "UnityEngine.Vector2", "UnityEngine.Quaternion" };
---
+++ Exiled.DevTools/Patches/MessageLoggingPatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Exiled.API.Features;
+using HarmonyLib;
+using Mirror;
+using Mirror.RemoteCalls;
+
+namespace Exiled.DevTools
+{
+	[HarmonyPatch(typeof(NetworkConnection), nameof(NetworkConnection.UnpackAndInvoke))]
+	public static class MessageLoggingPatch
+	{
+		public static void Prefix(NetworkConnection __instance, NetworkReader reader, int channelId)
+		{
+			var newreader = NetworkReaderPool.GetReader(reader.buffer);
+			if(!MessagePacking.Unpack(newreader, out var key)) return;
+
+			if(__instance.messageHandlers.TryGetValue(key, out var networkMessageDelegate) && networkMessageDelegate.Method.DeclaringType.IsGenericType)
+			{
+				string methodName = networkMessageDelegate.Method.DeclaringType.GetGenericArguments()[0].Name;
+				if(methodName == "RpcMessage" || methodName == "CommandMessage" || methodName == "UpdateVarsMessage") return;
+				if(DevTools.Instance.Config.DisabledLoggingNetworkMethods.Contains(methodName)) return;
+				Log.Debug($"[{methodName}]");
+			}
+			NetworkReaderPool.Recycle(newreader);
+		}
+	}
+}
