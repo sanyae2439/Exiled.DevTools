135fa4007c6c4740927cf37594456f43b94b3be8
初音早猫 <hatsunemiku_39@yahoo.co.jp>
2022-07-14 11:36:14 +0900

    2.2.4

--- Exiled.DevTools/Properties/AssemblyInfo.cs
+++ Exiled.DevTools/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      ビルド番号
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.3")]
-[assembly: AssemblyFileVersion("2.2.3")]
+[assembly: AssemblyVersion("2.2.4")]
+[assembly: AssemblyFileVersion("2.2.4")]
